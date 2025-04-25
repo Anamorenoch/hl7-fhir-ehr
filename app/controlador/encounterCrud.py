9e6a90e283225df6b1b1bd94dee8b29109fafccd
Anamorenoch <ANA.MORENO-C@mail.escuelaing.edu.co>
2025-04-24 23:51:21 -0500

    Update encounterCrud.py

--- app/controlador/encounterCrud.py
+++ app/controlador/encounterCrud.py
@@ -10,17 +10,20 @@ def WriteEncounter(encounter_dict: dict):
         # Validar que cumple el formato FHIR
         encounter = Encounter.model_validate(encounter_dict)
     except Exception as e:
-        print("Error validando encounter:", e)
+        print("Error validando encounter:", str(e))
         return f"errorValidating: {str(e)}", None
 
     # Convertir a diccionario limpio y guardar en MongoDB
     validated_encounter_json = encounter.model_dump()
 
     # Insertar encounter validado en MongoDB
-    result = collection.insert_one(validated_encounter_json)
-
-    if result:
-        inserted_id = str(result.inserted_id)
-        return "success", inserted_id
-    else:
+    try:
+        result = collection.insert_one(validated_encounter_json)
+        if result.inserted_id:
+            inserted_id = str(result.inserted_id)
+            return "success", inserted_id
+        else:
+            return "errorInserting", None
+    except Exception as e:
+        print(f"Error insertando encounter: {str(e)}")
         return "errorInserting", None
