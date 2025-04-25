789fef60d666b647a6375bb355174d930ff09f92
Anamorenoch <ANA.MORENO-C@mail.escuelaing.edu.co>
2025-04-24 20:57:18 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, HTTPException, Request
 import uvicorn
 from app.controlador.PatientCrud import GetPatientById,WritePatient,GetPatientByIdentifier
 from app.controlador.appointmentCrud import WriteAppointment
+from app.controlador.encounterCrud import WriteEncounter
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
