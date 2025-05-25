e409f991c281872950626643a8f13fbdf1cf8b59
Anamorenoch <ANA.MORENO-C@mail.escuelaing.edu.co>
2025-05-25 18:37:44 -0500

    Update appointmentCrud.py

--- app/controlador/appointmentCrud.py
+++ app/controlador/appointmentCrud.py
@@ -1,8 +1,6 @@
 from connection import connect_to_mongodb
 from bson import ObjectId
 from fhir.resources.appointment import Appointment
-from dateutil import parser
-from datetime import datetime, timezone, timedelta
 
 # Conexión a la colección de citas
 collection = connect_to_mongodb("CODIGOAPPOINTMENT", "PEDIR CITA")
@@ -15,26 +13,8 @@ def WriteAppointment(appointment_dict: dict):
         print("Error validando appointment:", e)
         return f"errorValidating: {str(e)}", None
 
-    # Convertir 'start' a datetime en UTC
-    start_time_str = appointment_dict.get("start")
-    start_time = parser.isoparse(start_time_str).astimezone(timezone.utc)
-    end_time = start_time + timedelta(minutes=30) 
-
-    #Verificar si ya existe una cita en ese rango
-    conflict = collection.find_one({
-        "start": {"$lt": end_time},
-        "end": {"$gt": start_time}
-    })
- 
-    if conflict:
-        return "duplicate", None
-
-    # Insertar si no hay conflicto
+    # Convertir a diccionario limpio y guardar en MongoDB
     validated_appointment_json = appointment.model_dump()
-    validated_appointment_json["start"] = start_time
-    validated_appointment_json["end"] = end_time
-
-
     result = collection.insert_one(validated_appointment_json)
 
     if result:
@@ -42,4 +22,3 @@ def WriteAppointment(appointment_dict: dict):
         return "success", inserted_id
     else:
         return "errorInserting", None
-
