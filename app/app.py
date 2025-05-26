f996b2527d8c677b30ce7df48aab15b2aabe2dc3
Anamorenoch <ANA.MORENO-C@mail.escuelaing.edu.co>
2025-05-25 19:03:38 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -50,10 +50,14 @@ async def add_patient(request: Request):
 async def add_appointment(request: Request):
     new_appointment_dict = dict(await request.json())
     status, appointment_id = WriteAppointment(new_appointment_dict)
+    
     if status == 'success':
-        return {"_id": appointment_id}  # Retorna el ID de la cita
+        return {"_id": appointment_id}
+    elif status == 'duplicate':
+        raise HTTPException(status_code=409, detail="Ya hay una cita en esa hora.")
     else:
         raise HTTPException(status_code=500, detail=f"Error de validación: {status}")
+
 
 @app.post("/encounter", response_model=dict)
 async def add_encounter(request: Request):
